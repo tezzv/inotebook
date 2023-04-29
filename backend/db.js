a4f1756d1942d484a0666f6273278659903b2cc1
tezzv <106775682+tezzv@users.noreply.github.com>
2023-04-29 15:33:33 +0530

    login

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017";
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI, () => {
-        console.log("Connected to Mongo Succesfully");
-    })
+    mongoose.connect('mongodb://localhost:27017/inotebook')
+        .then(() => {
+            console.log('Connected to MongoDB');
+        })
+        .catch((error) => {
+            console.error('Error connecting to MongoDB', error);
+        });
 }
+
 
 module.exports = connectToMongo;
