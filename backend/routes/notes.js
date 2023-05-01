0f03d4e351e0d0293ce3592d599e497d9626f06a
tezzv <106775682+tezzv@users.noreply.github.com>
2023-05-01 08:27:44 +0530

    update note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -21,7 +21,7 @@ router.post('/addnote', fetchuser, [
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
     try {
-        
+
         // If there are errors, return bad request and the errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -37,6 +37,40 @@ router.post('/addnote', fetchuser, [
         const savedNote = await note.save();
 
         res.json(savedNote);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+// ROUTE 2: Update an existing Note using: PUT "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    try {
+
+        const { title, description, tag } = req.body;
+        // create a newNote object
+
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        // Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not found") };
+
+        console.log(note.user.toString());
+        console.log(req.user.id);
+
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+
+        res.json({note});
 
     } catch (error) {
         console.error(error.message);
